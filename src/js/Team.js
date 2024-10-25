1e605998523ab04c0c5205c3aeb15ea377e5d8d5
Maksim Chalyh <nazgulius@yandex.ru>
2024-10-25 22:25:30 +0300

    set OK

--- src/js/Team.js
+++ src/js/Team.js
@@ -3,15 +3,23 @@ export default class Team {
     this.members = new Set();
   }
 
-  add() {
-
+  add(name) {  
+    this.members.forEach((item) => {
+      if (item === name) {
+        throw new Error('Такой персонаж уже есть');
+      }
+    });
+    this.members.add(name);
+    console.log([...this.members]);
   }
 
-  addAll() {
-
+  addAll(...name){
+    name.forEach((item)=>{
+      this.add(item);
+    });
   }
-  
+
   toArray() {
-
+    return Array.from(this.members);
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,11 @@
 import Team from './Team.js';
 
 console.log('Работает');
+
+const team = new Team();
+
+console.log(team.add('Вася'));
+console.log(team.add('Петя'));
+console.log(team.add('Оля'));
+console.log(team.toArray());
+
